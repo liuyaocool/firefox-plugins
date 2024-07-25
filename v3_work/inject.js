f79db14daeefbcf074fc8be85780cd188be4100b
liuyao <liuyao.cool.61@gmail.com>
2024-07-25 14:49:58 +0800

    v3 work

--- v3_work/inject.js
+++ v3_work/inject.js
@@ -48,6 +48,8 @@ if(location.href.indexOf('/login/login.init.do') >= 0
     document.getElementsByClassName('erweima')[0].click();
     document.getElementById('corpCode').focus();
     document.getElementsByClassName('yanClose')[0].click();
+    document.getElementById('language').value='zh_CN';
+    document.getElementById('language').dispatchEvent(new Event('change', { bubbles: true }))
 }
 
 if('jenkins.21tb.com' ==location.host) {
@@ -113,7 +115,37 @@ if(location.href.indexOf('/rtr') >= 0
     addTip(`<span style="color:#91671c;font-weight:bold;"> 右Alt </span>:前往5.0桌面`);
 }
 
-if (location.href.indexOf('/os/html/deskTop.init.do?goV5Desk=true') >= 0) {
+if (location.host == 'mail.21tb.com') {
+    document.onkeydown = function (ev) {
+        switch(ev.code) {
+            case 'AltRight':
+                break;
+            case 'AltLeft':
+                // 抄送按钮
+                if (document.getElementById('cc-link')) {
+                    document.getElementById('cc-link').click();
+                    return;
+                }
+                // 发送邮件页面也有此id 新邮件按钮
+                if (document.getElementById('rcmbtn109')) {
+                    document.getElementById('rcmbtn109').click();
+                    return;
+                }
+                break;
+            case 'Escape':
+                document.getElementById('composebody').focus();
+                break;
+            default: break;
+        }
+    }
+    addTip(`
+        <span style="color:#91671c;font-weight:bold;"> 左Alt </span>: 添加抄送/新邮件；
+        <span style="color:#91671c;font-weight:bold;"> Esc </span>:定位到输入；
+        <br>
+    `)
+}
+
+if (location.href.indexOf('/os/html/deskTop.init.do') >= 0) {
     const tbc_os_focus_search = () => {
         let tbc_os_sf = document.getElementsByClassName('tbc-os-btm-search-form')[0];
         tbc_os_sf.parentElement.classList.add('focus');
