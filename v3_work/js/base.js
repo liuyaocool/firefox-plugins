2c92e52728a54f82038d394f8abc002fae32f8db
liuyao <liuyao.cool.61@gmail.com>
2024-07-26 11:14:20 +0800

    v3 work

--- v3_work/js/base.js
+++ v3_work/js/base.js
@@ -1,6 +1,8 @@
 function addTip(innerHtml) {
+    let clz = 'ly-work-tbc-tip';
+    document.querySelectorAll(`.${clz}`).forEach(dvi => dvi.remove());
     let tip = document.createElement('div');
-    tip.classList.add('tbc-tip');
+    tip.classList.add(clz);
     tip.innerHTML = innerHtml;
     document.body.appendChild(tip);
 }
--- v3_work/js/nacos.js
+++ v3_work/js/nacos.js
@@ -1,16 +1,74 @@
-document.onkeydown = function (ev) {
+let keyHandler, editFocusIdx = 0;;
+window.addEventListener('hashchange', init);
+document.addEventListener('keydown', ev => keyHandler(ev));
+
+init();
+
+function init() {
+    switch(location.hash.substring(0, location.hash.indexOf('?'))) {
+        case "#/serviceManagement": 
+            keyHandler = defaultHandler;
+            addTip(`
+                <span class="ly-work-key"> Esc </span>: 定位第一搜索框；
+            `);
+            break;
+        case "#/configurationManagement": 
+            keyHandler = configManageHandler; 
+            addTip(`
+                <span class="ly-work-key"> Esc </span>: 定位第一搜索框；
+                <span class="ly-work-key"> AlrRight </span>: 聚焦‘编辑’；
+            `);
+            break;
+        default: 
+            addTip('');
+            break;
+    }
+}
+
+function defaultHandler(ev) {
     switch(ev.code) {
-        case 'Escape':
-            // let dom = document.querySelectorAll('.next-form')[0].children[1].querySelector('input');
-            let dom = document.querySelectorAll('.next-form input')[0];
-            dom.focus();
-            dom.select();
-            break
+        case 'Escape': 
+            focusFirstSearch();
+            break;
         default: break;
     }
 }
-addTip(`
-    <span style="color:#91671c;font-weight:bold;"> Esc </span>: 定位到 第一个搜索框
-`);
+
+function configManageHandler(ev) {
+    let selData = document.querySelectorAll('.ly-work-select');
+    switch(ev.code) {
+        case 'Escape': 
+            focusFirstSearch();
+            break;
+        case 'Enter': 
+            selData.length > 0 && selData[0].click();
+            break;
+        case 'AltRight':
+            if (selData.length == 0) {
+                editFocusIdx = 0;
+            } else {
+                selData.forEach(a => a.classList.remove('ly-work-select'));
+            }
+            let dataTr = document.querySelectorAll('table > tbody > tr');
+            editFocusIdx %= dataTr.length;
+            let editButton = dataTr[editFocusIdx++].children[4].querySelectorAll('a')[2];
+            editButton.classList.add('ly-work-select');
+            break;
+        default: break;
+    }
+}
+            // document.getElementById('container').focus();
+            // document.querySelectorAll('#container .view-line')[1].focus();
+            // document.querySelectorAll('#container .view-line')[1].click();
+
+
+
+function focusFirstSearch() {
+    let dom = document.querySelectorAll('.next-form input')[0];
+    if (dom) {
+        dom.focus();
+        dom.select();            
+    }
+}
 
 console.log('work nacos inject');
