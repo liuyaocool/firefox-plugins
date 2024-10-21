a7442a9396b3df37b9e4b58200755846177143c7
liuyao <liuyao.cool.61@gmail.com>
2024-10-21 10:14:03 +0800

    work

--- v3_work/js/openframe.js
+++ v3_work/js/openframe.js
@@ -1,6 +1,6 @@
 if(window.parent != window) {
     let btn = document.createElement('div');
-    btn.style = 'position: fixed; top: 0; width: 100%; z-index: 99999999; text-align: center;';
+    btn.style = 'position: fixed; top: 0; z-index: 99999999;left:calc(50% - 31px);';
     btn.innerHTML = '<button id="ly_work_open_frame" style="line-height:12px;">新窗打开</button>';
     document.body.appendChild(btn);
     document.getElementById('ly_work_open_frame').onclick = e => {
